d29b86608769e446fc3c4d72eb270ed36504797b
Superbizons <bednarskiantek@gmail.com>
2018-04-22 00:38:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+        <meta charset="utf-8"/>
         <title>Bucket</title>
     </head>
     <body>
-
+        <form>
+            Login: <br/> <input type="text" name="loginField"/> <br/>
+            Password: <br/> <input type="password" name="passwordField"> <br/>
+            <input type="button" value="Login"/>
+        </form>
     </body>
 </html>
